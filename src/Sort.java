03b6f01ef8246f1c5f31abfddef909facec938ce
kenny_xue3 <kennyx3@nycstudents.net>
2022-02-17 13:29:27 -0500

    finished

--- src/Sort.java
+++ src/Sort.java
@@ -1,51 +1,71 @@
 import java.util.ArrayList;
 
-public class Sort
-{
-  // Part A.  Which Sort is Which?
-  public static void firstSortingAlgorithm(int[] elements)
-  {
-    for (int j = 0; j < elements.length - 1; j++)
-    {
-      int minIndex = j;
-      for (int k = j + 1; k < elements.length; k++)
-      {      
-        if (elements[k] < elements[minIndex])
-        {
-          minIndex = k;
+public class Sort {
+    // Part A.  Which Sort is Which?
+    public static void selectionSort(int[] elements) {
+        int count = 0;
+        for (int j = 0; j < elements.length - 1; j++) {
+            int minIndex = j;
+            for (int k = j + 1; k < elements.length; k++) {
+                count++;
+                if (elements[k] < elements[minIndex]) {
+                    minIndex = k;
+                }
+            }
+            int temp = elements[j];
+            elements[j] = elements[minIndex];
+            elements[minIndex] = temp;
         }
-      }
-      int temp = elements[j];
-      elements[j] = elements[minIndex];
-      elements[minIndex] = temp;
+        System.out.println("Selection sort, number of iterations: " + count);
     }
-  }
 
-  // Part A.  Which Sort is Which?
-  public static void secondSortingAlgorithm(int[] elements)
-  { 
-    for (int j = 1; j < elements.length; j++)
-    {
-      int temp = elements[j];
-      int possibleIndex = j;
-      while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
-      {     
-        elements[possibleIndex] = elements[possibleIndex - 1];
-        possibleIndex--;
-      }
-      elements[possibleIndex] = temp;
+    // Part A.  Which Sort is Which?
+    public static void insertionSort(int[] elements) {
+        int count = 0;
+        for (int j = 1; j < elements.length; j++) {
+            int temp = elements[j];
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp < elements[possibleIndex - 1]) {
+                count++;
+                elements[possibleIndex] = elements[possibleIndex - 1];
+                possibleIndex--;
+            }
+            elements[possibleIndex] = temp;
+        }
+        System.out.println("Insertion sort, number of iterations: " + count);
     }
-  }
-  
-  // Part C.  Sorting a 1000-word list!
-  public static void selectionSortWordList(ArrayList<String> words)
-  {
-    /* TO BE IMPLEMENTED IN PART C */
-  }
 
-  // Part C.  Sorting a 1000-word list!
-  public static void insertionSortWordList(ArrayList<String> words)
-  {    
-    /* TO BE IMPLEMENTED IN PART C */
-  }
+    // Part C.  Sorting a 1000-word list!
+    public static void selectionSortWordList(ArrayList<String> words) {
+        int count = 0;
+        for (int i = 0; i < words.size() - 1; i++) {
+            int smallestIdx = i;
+            for (int j = i + 1; j < words.size(); j++) {
+                count++;
+                if (words.get(j).compareTo(words.get(smallestIdx)) < 0) {
+                    smallestIdx = j;
+                }
+            }
+            String temp = words.get(i);
+            words.set(i, words.get(smallestIdx));
+            words.set(smallestIdx, temp);
+        }
+        System.out.println("Selection sort, number of iterations: " + count);
+    }
+
+    // Part C.  Sorting a 1000-word list!
+    public static void insertionSortWordList(ArrayList<String> words) {
+        int count = 0;
+        for (int i = 1; i < words.size(); i++) {
+            String temp = words.get(i);
+            int possibleIndex = i;
+            while (possibleIndex > 0 && temp.compareTo(words.get(possibleIndex - 1)) < 0) {
+                count++;
+                words.set(possibleIndex, words.get(possibleIndex - 1));
+                possibleIndex--;
+            }
+            words.set(possibleIndex, temp);
+        }
+        System.out.println("Insertion sort, number of iterations: " + count);
+    }
 }
--- src/SortTester.java
+++ src/SortTester.java
@@ -1,17 +1,17 @@
 import java.util.Arrays;
 
-public class SortTester
-{
-  public static void main(String[] args)
-  {
-    /* --- SELECTION SORT --- */
-    int[] arr1 = {3, 86, -20, 14, 40, 19, -4, 100, 89, 13, 54, 12, 77, -5, 0, 6}; 
-    Sort.selectionSort(arr1);
-    System.out.println(Arrays.toString(arr1));
- 
-    /* --- INSERTION SORT --- */ 
-    int[] arr2 = {3, 86, -20, 14, 40, 19, -4, 100, 89, 13, 54, 12, 77, -5, 0, 6};    
-    Sort.insertionSort(arr2);
-    System.out.println(Arrays.toString(arr2));
-  }
+public class SortTester {
+    public static void main(String[] args) {
+        /* --- SELECTION SORT --- */
+        int[] arr1 = {-232, 23, 6, 8, 1, 40,10, 7, -50670, 2, 655, 4, 3, -400, 0, 5, 123, 9};
+        System.out.println(arr1.length);
+        Sort.selectionSort(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+        /* --- INSERTION SORT --- */
+        int[] arr2 = {-232, 23, 6, 8, 1, 40,10, 7, -50670, 2, 655, 4, 3, -400, 0, 5, 123, 9};
+        System.out.println(arr2.length);
+        Sort.insertionSort(arr2);
+        System.out.println(Arrays.toString(arr2));
+    }
 }
